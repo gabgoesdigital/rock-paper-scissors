dcabd8a12f5e81a3e190096f0d185cdbd737f31e
Gab <gabgoesdigital@users.noreply.github.com>
2025-01-11 21:40:46 +0600

    Add COmputer Choice
    Adding a Function that generates a random number
    between 0 and 2, assign each value to one of
    the rock, paper, scissors.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,14 @@
 function getComputerChoice() {
-    let rock = "rock";
-    let scissors = "scissors";
-    let paper = "paper";
+    
  let randomNumber = Math.floor(Math.random()* 3);
 
 if (randomNumber === 0) {
-    return rock;}
+    return "rock";}
     else if (randomNumber ===1) {
-        return scissors;}
+        return "scissors";}
         else {
-            return paper;
+            return "paper";
         }
 }
 
-console.log(getComputerChoice());
+
