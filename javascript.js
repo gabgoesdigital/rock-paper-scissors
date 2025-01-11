55d04b18930768ff347d4f0faeeb964506f4cc24
Gab <gabgoesdigital@users.noreply.github.com>
2025-01-11 18:18:13 +0600

    Add computer choice
    by console log the function it will randomly log
    paper scissor or rock.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("HEllo World");
+function getComputerChoice() {
+    let rock = "rock";
+    let scissors = "scissors";
+    let paper = "paper";
+ let randomNumber = Math.floor(Math.random()* 3);
+
+if (randomNumber === 0) {
+    return rock;}
+    else if (randomNumber ===1) {
+        return scissors;}
+        else {
+            return paper;
+        }
+}
+
+console.log(getComputerChoice());
